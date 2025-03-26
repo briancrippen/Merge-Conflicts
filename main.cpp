fb2048674d063da1c77e068868d91d4b0aec875c
briancrippen <bdcrippen@nic.edu>
2025-03-26 20:42:42 +0000

    Added function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b);
 int main()
 {
     cout << "Hello World" << endl;
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
