3daad7b0e7607f4792583dd5ccf38f64171d7049
Brian Crippengit config --global user.email bdcrippen@nic.edugit clone REPO_URL Lab2git config --global user.name Brian <bdcrippen@nic.edu>
2025-03-26 13:49:12 -0700

    Added say function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
 int main()
 {
     cout << "Greeting...." << endl;
     cout << "Hello World" << endl;
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
