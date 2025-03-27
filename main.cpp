843520eb46c7d1a88cf37ca60834928aee8ea55e
Brian Crippengit config --global user.email bdcrippen@nic.edugit clone REPO_URL Lab2git config --global user.name Brian <bdcrippen@nic.edu>
2025-03-27 15:34:59 -0700

    Restore main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void say(string s);
 int add(int a, int b);
 int main()
 {
-
+    cout << "Greeting...." << endl;
+    cout << "Hello World" << endl;
     return 0;
-}
-
-void say(string s)
-{
-    cout << s << endl;
 }
 
 int add(int a, int b)
